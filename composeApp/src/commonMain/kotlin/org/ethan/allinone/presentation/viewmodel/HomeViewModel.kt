4d928b2bdd71b635aa6f1ee6a9ad0abf933b3997
Ethan Banh <banhlevuthan@gmail.com>
2025-11-09 12:40:06 -0500

    fix error data class

--- composeApp/src/commonMain/kotlin/org/ethan/allinone/presentation/viewmodel/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/ethan/allinone/presentation/viewmodel/HomeViewModel.kt
@@ -6,9 +6,9 @@ import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import org.ethan.allinone.data.model.ProductDto
 import org.ethan.allinone.data.model.ProductsResponse
 import org.ethan.allinone.data.reposittory.HomeRepository
-import org.ethan.allinone.domain.model.Product
 import org.ethan.allinone.presentation.state.CommonUiState
 
 class HomeViewModel(private val homeRepository: HomeRepository) : BaseViewModel() {
@@ -16,8 +16,8 @@ class HomeViewModel(private val homeRepository: HomeRepository) : BaseViewModel(
     private val _uiStateProductList = MutableStateFlow<CommonUiState<ProductsResponse?>>(CommonUiState.Loading)
     val uiStateProductList: StateFlow<CommonUiState<ProductsResponse?>> = _uiStateProductList
 
-    private val _uiStateProductDetail = MutableStateFlow<CommonUiState<Product?>>(CommonUiState.Loading)
-    val uiStateProductDetail: StateFlow<CommonUiState<Product?>> = _uiStateProductDetail
+    private val _uiStateProductDetail = MutableStateFlow<CommonUiState<ProductDto?>>(CommonUiState.Loading)
+    val uiStateProductDetail: StateFlow<CommonUiState<ProductDto?>> = _uiStateProductDetail
 
     fun getProductList() = CoroutineScope(Dispatchers.IO).launch {
         fetchData(_uiStateProductList) { homeRepository.getProducts() }
